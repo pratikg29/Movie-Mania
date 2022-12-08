9d71cb3694d6a394d991178295ba663d2e3991d8
Pratik Gadhesariya <pratikgadhesariya29@gmail.com>
2022-12-08 11:41:15 +0530

    minor change

---
+++ Movie Mania/Movie Mania/Common Views/TestingView.swift
@@ -0,0 +1,20 @@
+//
+//  TestingView.swift
+//  Movie Mania
+//
+//  Created by Pratik on 08/12/22.
+//
+
+import SwiftUI
+
+struct TestingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TestingView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestingView()
+    }
+}
